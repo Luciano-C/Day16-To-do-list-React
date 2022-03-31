faed93d992d92ef613b76df9e040f4c4ef5f862b
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-03-31 16:27:00 -0300

    Arreglos menores

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
-  
+
   const [itemList, setItem] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [listItemsHTML, setListItemsHTML] = useState([]);
- 
-  
-  
-  
+
+
+
+
   const addItem = () => {
     itemList.push(inputValue);
-    let itemsToAdd = itemList.map((x,i) => <li key={i}>{x}</li>);
+    let itemsToAdd = itemList.map((x, i) => <li key={i}>{x}</li>);
     setListItemsHTML(itemsToAdd);
+    setInputValue("");
   }
-  
 
-  
+
+
   return (
     <div className="globalContainer">
       <h1>To do</h1>
-      <div className = "paperContainer">
-        <input id="inputToDo" type="text" onChange={e => setInputValue(e.target.value)} onKeyPress = {e => {if(e.key === "Enter"){addItem()}}}   value={inputValue} placeholder="What needs to be done?"/>
-        <div id="paper-1"><ul>{listItemsHTML}</ul></div>
+      <div className="paperContainer">
+        <input id="inputToDo" type="text" onChange={e => setInputValue(e.target.value)} onKeyPress={e => { if (e.key === "Enter") { addItem() } }} value={inputValue} placeholder="What needs to be done?" />
+        <div id="paper-1">
+          <ul>{listItemsHTML}</ul>
+          <p id='itemsLeft'>{itemList.length} items left</p>
+        </div>
         <div id="paper-2"></div>
         <div id="paper-3"></div>
       </div>
